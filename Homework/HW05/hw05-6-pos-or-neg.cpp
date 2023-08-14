436810ba20712e6ddba2647623ca1f6dbccb48ec
your name <fifasisa36@gmail.com>
2023-08-14 16:31:28 +0700

    hw5-6

--- Homework/HW05/hw05-6-pos-or-neg.cpp
+++ Homework/HW05/hw05-6-pos-or-neg.cpp
@@ -16,3 +16,16 @@
         Negative
 
 */
+#include <stdio.h>
+int main(){
+    int i;
+    printf("Test case:\n");
+    printf("Input :\n");
+    scanf("%d",&i);
+    if (i > 0){
+        printf("Output:\n Positive");
+    }if(i < 0){
+        printf("Output:\n Negative");
+    }
+    
+}
